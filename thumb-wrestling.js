3cd6e4c9768dd4a33be63af8110351875646de92
Michael Gagnon <mikegagnon@gmail.com>
2016-12-14 15:00:37 -0700

    Movement

--- thumb-wrestling.js
+++ thumb-wrestling.js
@@ -2,6 +2,9 @@
 var numRows = 10;
 var numCols = 10;
 
+var redRow = 2;
+var redCol = 2;
+
 document.onkeydown = keydown;
 
 function createThumbWrestling(boardId) {
@@ -16,6 +19,39 @@ function createThumbWrestling(boardId) {
     }
 
     $("#cell-2-2").append("<img id='red-arrow' src='red-arrow.png' class='up'>")
+}
+
+function drdc(direction) {
+
+    if (direction == "up") {
+        return [-1, 0];
+    } else if (direction == "down") {
+        return [1, 0];
+    } else if (direction == "left") {
+        return [0, -1];
+    } else if (direction == "right") {
+        return [0, 1];
+    } else {
+        console.error("Bad direction: " + direction)
+    }
+
+}
+
+function move(direction) {
+    $("#red-arrow").removeClass("up down left right");
+    $("#red-arrow").addClass(direction);
+
+    var [dr, dc] = drdc(direction);
+
+    $("#red-arrow").remove();
+
+    redRow += dr;
+    redCol += dc;
+
+    var cellId = "#cell-" + redRow + "-" + redCol;
+
+    $(cellId).append("<img id='red-arrow' src='red-arrow.png' class='"+ direction + "'>")
+
 }
 
 function keydown(event) {
@@ -39,7 +75,6 @@ function keydown(event) {
         return;
     }
 
-    $("#red-arrow").removeClass("up down left right");
-    $("#red-arrow").addClass(direction);
+    move(direction);
 }
 
