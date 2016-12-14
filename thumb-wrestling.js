4ed051a186cc420b93add36bd6974217cced960c
Michael Gagnon <mikegagnon@gmail.com>
2016-12-14 14:52:21 -0700

    Respond to keypress events

--- thumb-wrestling.js
+++ thumb-wrestling.js
@@ -2,6 +2,8 @@
 var numRows = 10;
 var numCols = 10;
 
+document.onkeydown = keydown;
+
 function createThumbWrestling(boardId) {
 
     for (var row = 0; row < numRows; row++) {
@@ -13,5 +15,31 @@ function createThumbWrestling(boardId) {
         }
     }
 
-    $("#cell-2-2").append("<img src='red-arrow.png'>")
+    $("#cell-2-2").append("<img id='red-arrow' src='red-arrow.png' class='up'>")
 }
+
+function keydown(event) {
+
+    // diable browser scrolling on arrow keys
+    if([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+        event.preventDefault();
+    }
+
+    var direction;
+
+    if (event.keyCode == '38') {
+        direction = "up";
+    } else if (event.keyCode == '40') {
+        direction = "down";
+    } else if (event.keyCode == '37') {
+        direction = "left";
+    } else if (event.keyCode == '39') {
+        direction = "right";
+    } else {
+        return;
+    }
+
+    $("#red-arrow").removeClass("up down left right");
+    $("#red-arrow").addClass(direction);
+}
+
