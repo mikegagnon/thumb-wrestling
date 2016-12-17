a1b121f075a804abcfcd4d79a59a7dfa97c6a610
Michael Gagnon <mikegagnon@gmail.com>
2016-12-16 18:01:12 -0700

    Lecture 2, step 3

---
+++ lecture02/step03/thumb-wrestling.js
@@ -0,0 +1,37 @@
+
+function getPlayerMovment(keyCode) {
+
+    var keyCodeMap = {
+        87: ["green", "up"],
+        83: ["green", "down"],
+        65: ["green", "left"],
+        68: ["green", "right"],
+        38: ["red", "up"],
+        40: ["red", "down"],
+        37: ["red", "left"],
+        39: ["red", "right"]
+    };
+
+    return keyCodeMap[keyCode];
+}
+
+function keydown(event) {
+
+    var playerMovement = getPlayerMovment(event.keyCode);
+
+    // If the user pressed a key we're uninterested in
+    if (playerMovement == undefined) {
+        return;
+    }
+
+    var [color, direction] = playerMovement;
+
+    // disable browser scrolling on arrow keys
+    if (color == "red") {
+        event.preventDefault();
+    }
+
+    alert(color + " " + direction);
+}
+
+document.onkeydown = keydown;
